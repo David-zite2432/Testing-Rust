db83c8371f3a9df91ac53ad62bf5cf0319073f84
David Olivero <daniolidavid32@gmail.com>
2023-03-14 16:53:29 -0500

    Providers struct and default provider added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+struct Providers {
+    companie: String,
+    products: Vec<String>,
+    local: String,
+    products_amount: u32
+}
+
+impl Providers {
+    fn struct_fill(companie: &str, products: Vec<String>, local: &str, products_amount: u32) -> Providers {
+        Providers { 
+            companie: companie.to_string(), 
+            products,
+            local: local.to_string(), 
+            products_amount 
+        }
+    }
+
+    fn show_elements(self) {
+        print!(
+            "[{} provider]\n> Productos que provee: {:?}\n> Local: {}\n> Cantidad de productos al día: {}\n", 
+            self.companie, self.products, self.local, self.products_amount
+        );
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Wellcome to [A company project!]");
+
+    let provider_a = Providers::struct_fill(
+        "B companie", 
+        vec![String::from("Café"), String::from("Leche"), String::from("Harina")], 
+        "Sucre", 
+        100
+    );
+
+    Providers::show_elements(provider_a);
 }
